8e95c7efaabfbabd2ffaead4b60c295902012a1a
Aliris Escobar <aliris.escobar@gmail.com>
2026-03-10 13:05:02 +0100

    respuestas hasta la Nro 24

--- Promo_65_Evaluacion_Final_Mod_2_Aliris_Escobar.sql
+++ Promo_65_Evaluacion_Final_Mod_2_Aliris_Escobar.sql
@@ -147,5 +147,137 @@ SELECT c.name, f.title
 		ON f.film_id = fc.film_id
 	WHERE c.name = "Family";
     
+    -- 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.
     
+	SELECT a.actor_id, a.first_name, a.last_name, f.title -- primero buscamos la ruta
+    FROM actor AS a
+    INNER JOIN film_actor as fa
+    ON a.actor_id = fa.actor_id
+    INNER JOIN film as f
+    ON fa.film_id =f.film_id;
+    
+
+
+    -- Query final, hacemos un count 
+SELECT a.first_name, a.last_name, COUNT(f.title) AS Total_Pelis
+    FROM actor AS a
+    LEFT JOIN film_actor as fa
+			ON a.actor_id = fa.actor_id
+    INNER JOIN film as f
+			ON fa.film_id =f.film_id
+        GROUP BY a.first_name, a.last_name
+        HAVING COUNT(f.title) > 10;
+        
+-- 19. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film.
 	
+SELECT title, length, rating
+	FROM film
+	WHERE rating = "R" AND length < 120; -- Convertimos horas a minutos
+    
+-- 20.  Encuentra las categorías que tienen un promedio superior a 120 minutos y muestra el nombre de la categoría junto con el promedio de duración.
+
+	SELECT c.name, AVG(length) -- Misma ruta de la 17 pero con AVG
+	FROM category AS c
+	INNER JOIN film_category AS fc
+		ON fc.category_id = c.category_id
+	INNER JOIN film AS f
+		ON f.film_id = fc.film_id
+	GROUP BY c.name
+    HAVING AVG(f.length) > 120;
+
+-- 21. actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad de películas en las que han actuado.
+
+SELECT a.first_name, COUNT(f.title) AS Total_Pelis -- Similar a la 18, solo nos piden nombre
+    FROM actor AS a
+    LEFT JOIN film_actor as fa
+			ON a.actor_id = fa.actor_id
+    INNER JOIN film as f
+			ON fa.film_id =f.film_id
+        GROUP BY a.first_name, a.last_name
+        HAVING COUNT(f.title) > 4;
+        
+-- 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para encontrar los rental_ids con una duración superior a 5 días y luego selecciona las películas correspondientes.
+-- Primero resuelvo la sub consulta, veo que el formato de fecha en rental es date time, pruebo a hacer  DATEDIFF
+
+SELECT r.rental_id, DATEDIFF(return_date, rental_date) AS num_dias
+FROM rental AS r
+WHERE DATEDIFF(return_date, rental_date) > 5;
+
+-- A mi me parece mas bonito asi, sin subconsulta, pero con 2 inner joins porque puedo ver todos los datos 
+
+SELECT f.title, r.rental_id, DATEDIFF(return_date, rental_date) AS num_dias
+	FROM film AS f
+	INNER JOIN inventory AS i
+		ON f.film_id = i.film_id
+	INNER JOIN rental AS r 
+		ON i.inventory_id = r.inventory_id
+		WHERE DATEDIFF(r.return_date, r.rental_date) > 5;
+        
+-- Query final con subconsulta
+SELECT f.title
+FROM film AS f
+INNER JOIN inventory AS i 
+	ON f.film_id = i.film_id
+WHERE i.inventory_id IN (
+    SELECT r.inventory_id
+    FROM rental AS r
+    WHERE DATEDIFF(r.return_date, r.rental_date) > 5);
+    
+    
+-- 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
+
+SELECT a.first_name, a.last_name, c.name -- Hare un innerjoin para toda la ruta de modo que pueda tener todos los datos 
+    FROM actor AS a
+    LEFT JOIN film_actor as fa
+		ON a.actor_id = fa.actor_id
+    INNER JOIN film as f
+		ON fa.film_id =f.film_id
+	INNER JOIN film_category as fc
+		ON f.film_id = fc.film_id
+	INNER JOIN category as c
+		ON c.category_id = fc.category_id;
+        
+-- Ahora la subconsulta
+SELECT name FROM category
+	WHERE name != 'Horror';
+
+     -- query final con todo
+SELECT a.first_name, a.last_name, fc.category_id
+FROM actor AS a
+INNER JOIN film_actor AS fa 
+ON a.actor_id = fa.actor_id
+INNER JOIN film AS f 
+ON fa.film_id = f.film_id
+INNER JOIN film_category AS fc 
+ON f.film_id = fc.film_id
+WHERE fc.category_id NOT IN 
+(SELECT category_id FROM category
+	WHERE name = 'Horror');
+
+-- Hecha la subconsulta, me gusta mas la solucion con joins
+  
+SELECT a.first_name, a.last_name, c.name -- Hare un innerjoin para toda la ruta de modo que pueda tener todos los datos 
+    FROM actor AS a
+    INNER JOIN film_actor as fa
+		ON a.actor_id = fa.actor_id
+    INNER JOIN film as f
+		ON fa.film_id =f.film_id
+	INNER JOIN film_category as fc
+		ON f.film_id = fc.film_id
+	INNER JOIN category as c
+		ON c.category_id = fc.category_id
+        WHERE c.name != 'Horror';
+        
+-- 24. Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla film.
+
+	SELECT f.title, c.name, f.length
+    FROM film AS f
+	INNER JOIN film_category as fc
+		ON f.film_id = fc.film_id
+	INNER JOIN category as c
+		ON c.category_id = fc.category_id
+	WHERE c.name  = "Comedy" AND f.length > 180;
+    
+    -- 25.Encuentra todos los actores que han actuado juntos en al menos una película. La consulta debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.	
+
+
